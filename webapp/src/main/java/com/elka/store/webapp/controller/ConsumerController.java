7070794e9b62fcf13d45eeabd9fee9519be42e03
Kuzmin Denis <dkuzmin@wiley.com>
2015-04-06 19:11:21 +0300

    configuring start

--- webapp/src/main/java/com/elka/store/webapp/controller/ConsumerController.java
+++ webapp/src/main/java/com/elka/store/webapp/controller/ConsumerController.java
@@ -3,17 +3,17 @@ package com.elka.store.webapp.controller;
 import com.elka.shop.domain.Consumer;
 import com.elka.shop.services.inter.ConsumerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Controller
+@RestController
 public class ConsumerController {
     @Autowired
     private ConsumerService consumerService;
 
-   @RequestMapping(value = "/elka/store")
+   @RequestMapping(value = "/first")
     public Consumer getConsumer(final HttpServletRequest request){
         return consumerService.getConsumerByLogin("ivan");
     }
